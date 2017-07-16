8274e582743a79adcd9db80e236b0a527c97cc3a
brentsouthern93 <southern.brent@yahoo.com>
2017-07-15 23:14:24 -0400

    Add files via upload

---
+++ forloops.js
@@ -0,0 +1,36 @@
+for(var i = 0; i < 16; i+=8) {
+	console.log(i);
+}
+
+
+var str = "ahceclwlxo";
+
+for(var i = 1; i < str.length; i+=2) {
+	console.log(str[i]);
+}
+
+console.log("Print all numbers between -10 and 19")
+for(var i = -10; i < 20; i++) {
+	console.log(i)
+}
+
+console.log("Print all even numbers between 10 and 40")
+for(var i = 10; i <= 40; i ++) {
+	if(i % 2 === 0) {
+		console.log(i);
+	}
+}
+
+console.log("Print all odd numbers between 300 and 333")
+for(var i = 300; i <= 333; i ++) {
+	if(i % 2 !== 0) {
+		console.log(i);
+	}
+}
+
+console.log("Print all numbers divisible by 5 and 50")
+for(var i = 5; i <= 50; i ++) {
+	if(i % 5 === 0 && i % 3 === 0) {
+		console.log(i);
+	}
+}
